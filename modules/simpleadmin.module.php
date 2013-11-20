e3da9238b8e88850eb3830934547e78fac966853
David Beuchert <Fake@me>
2013-11-20 18:15:33 +0100

    Optimized simpleadmin module and added join cmd

--- modules/simpleadmin.module.php
+++ modules/simpleadmin.module.php
@@ -1,33 +1,61 @@
 <?php
     class mod_simpleadmin {
-    
-        function __construct (&$core,$id) {
+
+        /**
+         * @var IRCCore
+         */
+        protected $core;
+
+        /**
+         * @var string
+         */
+        protected $id;
+
+        /**
+         * @param IRCCore $core
+         * @param string $id
+         */
+
+        function __construct ($core,$id) {
         
-            $this -> core = &$core;
+            $this -> core = $core;
             $this -> id   = $id;
+
+            $bind = Array(
+                '!*',
+                'JOIN'
+            );
             
-            $core -> register ( 'PRIVMSG','!*',true, $this->id );
+            $core -> register ('PRIVMSG', $bind,true, $this->id );
         
         }
         
-        function in ( $in ) {
+        function in ($in) {
             $core = $this -> core;
         
             if ( has_flags ( $in['sender'], $core -> cvar ( 'ADM', 'admin' ) ) ) {
             
-                $cmd = strtoupper ( substr ( $in['atext'][0], 1 ) );
+                $cmd  = strtoupper ( substr ( $in['atext'][0], 1 ) );
+                $args = array_slice($in['atext'], 1);
                 
-                if ( $cmd == 'ACTION' ) {
-                
-                    $core->action ( $in['args'][0], implode ( ' ', array_slice ( $in['atext'], 1 ) ) );
-                
+                switch($cmd) {
+                    case 'ACTION':
+                        $core->action ( $in['args'][0], implode ( ' ', array_slice ( $in['atext'], 1 ) ) );
+                        break;
+
+                    case 'JOIN':
+                        foreach ($args as $chan) {
+                            $core->write('JOIN ' . $chan);
+                        }
+                        break;
                 }
                 
             }
             else {
-            
-                $core -> notice ( $in['sender']['nick'], '%COMMAND_PERMISSION_DENIED%' );
-                
+
+                if (isset($in['text']['priv'])) {
+                    $core -> notice ( $in['sender']['nick'], '%COMMAND_PERMISSION_DENIED%' );
+                }
             }
             
         }
