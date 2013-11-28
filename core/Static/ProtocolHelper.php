c01785bce30a7181cbe29a3867094eab6237e46c
David Beuchert <Fake@me>
2013-11-28 15:46:50 +0100

    Moved maskMatch function to ProtocolHelper

--- core/Static/ProtocolHelper.php
+++ core/Static/ProtocolHelper.php
@@ -12,4 +12,49 @@
             return preg_match('/([#&][^\x07\x2C\s]{0,200})/', $channel);
         }
 
+        /**
+         * Matches a user identifier mask
+         * @param $mask
+         * @param $haystack
+         * @return bool
+         */
+        public static function doesMaskMatch($mask, $haystack) {
+            if ($mask == $haystack) {
+                return true;
+            }
+
+            // the simple one doesn't match? no problem, maybe it matches with a simple wildcard
+            $nick  = substr($mask, 0, strpos($mask, '!'));
+            $host  = substr($mask, (strpos($mask, '@') + 1));
+            $ident = str_replace('@' . $host, '', substr($mask, (strpos($mask, '!') + 1)));
+
+            $h_nick  = substr($haystack, 0, strpos($haystack, '!'));
+            $h_host  = substr($haystack, (strpos($haystack, '@') + 1));
+            $h_ident = str_replace('@' . $h_host, '', substr($haystack, (strpos($haystack, '!') + 1)));
+
+            if ($nick == $h_nick || $nick == '*' || $h_nick == '*') {
+                if ($host == $h_host || $host == '*' || $h_host == '*') {
+                    if ($ident == $h_ident || $ident == '*' || $h_ident == '*') {
+                        return true;
+                    }
+                }
+            }
+
+            // still no match? try regex and complex wildcards.
+            $h_nick  = str_replace('*', '[^!]+', $h_nick);
+            $h_nick  = str_replace('?', '(\S{1})', $h_nick);
+            $h_ident = str_replace('*', '[^@]+', $h_ident);
+            $h_ident = str_replace('?', '(\S{1})', $h_ident);
+            $h_host  = str_replace('*', '(.*)', $h_host);
+            $h_host  = str_replace('?', '(\S{1})', $h_host);
+
+            if (preg_match('/' . $h_nick . '!' . $h_ident . '@' . $h_host . '/', $mask)) {
+                return true;
+            }
+
+            // if theres still no match, there is nothing to match. so return false.
+            return false;
+
+        }
+
     }
--- includes/functions.lib.php
+++ includes/functions.lib.php
@@ -496,55 +496,18 @@
         }
     
     }
-    function does_match($mask, $haystack) {
-    
-        if ( $mask == $haystack ) {
-        
-            return true;
-            
-        }
-    
-        // the simple one doesn't match? no problem, maybe it matches with a simple wildcard
-        $nick  = substr ( $mask, 0, strpos ( $mask, '!' ) );
-        $host  = substr ( $mask, ( strpos ( $mask, '@' ) + 1 ) );
-        $ident = str_replace ( '@' . $host, '', substr ( $mask, ( strpos ( $mask, '!' ) + 1 ) ) );
-        
-        $h_nick  = substr ( $haystack, 0, strpos ( $haystack, '!' ) );
-        $h_host  = substr ( $haystack, ( strpos ( $haystack, '@' ) + 1 ) );
-        $h_ident = str_replace ( '@' . $h_host, '', substr ( $haystack, ( strpos ( $haystack, '!' ) + 1 ) ) );
-        
-        if ( $nick == $h_nick || $nick == '*' || $h_nick == '*' ) {
-        
-            if ( $host == $h_host || $host == '*' || $h_host == '*' ) {
-            
-                if ( $ident == $h_ident || $ident == '*' || $h_ident == '*' ) {
-                
-                    return true;
-                
-                }
-                
-            }
-            
-        }
-        
-        // still no match? try regex and complex wildcards.
-        $h_nick  = str_replace ( '*', '[^!]+', $h_nick );
-        $h_nick  = str_replace ( '?', '(\S{1})', $h_nick );
-        $h_ident = str_replace ( '*', '[^@]+', $h_ident );
-        $h_ident = str_replace ( '?', '(\S{1})', $h_ident );
-        $h_host  = str_replace ( '*', '(.*)', $h_host );
-        $h_host  = str_replace ( '?', '(\S{1})', $h_host );
-        
-        if ( preg_match ( '/' . $h_nick . '!' . $h_ident . '@' . $h_host . '/', $mask ) ) {
-        
-            return true;
-        
-        }
-        
-        // if theres still no match, there is nothing to match. so return false.
-        return false;
 
+    /**
+     * @deprecated
+     * @see ProtocolHelper::doesMaskMatch
+     * @param $mask
+     * @param $haystack
+     * @return bool
+     */
+    function does_match($mask, $haystack) {
+        return ProtocolHelper::doesMaskMatch($mask, $haystack);
     }
+
     function parse_vars ( $str, $opt, $seperated='%' ) {
     
         if ( !is_array ( $opt ) ) {
