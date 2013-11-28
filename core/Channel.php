920d28229da1236c4bd4b500bf74265693a1799c
David Beuchert <Fake@me>
2013-11-28 17:29:10 +0100

    Added phpdoc

--- core/Channel.php
+++ core/Channel.php
@@ -36,29 +36,50 @@
          */
         protected $ops = Array();
 
-
+        /**
+         * Adds a user as OP to the channel
+         *
+         * @param User $op
+         */
         public function addOp(User $op) {
             if (!in_array($op, $this->ops)) {
                 $this->ops[] = $op;
             }
         }
 
+        /**
+         * Adds a user as voiced to the channel
+         *
+         * @param User $voiced
+         */
         public function addVoice(User $voiced) {
             if (!in_array($voiced, $this->voiced)) {
                 $this->voiced[] = $voiced;
             }
         }
 
+        /**
+         * Adds a normal user to the channel
+         *
+         * @param User $user
+         */
         public function addUser(User $user) {
             if (!in_array($user, $this->users)) {
                 $this->users[] = $user;
             }
         }
 
+        /**
+         * Sets the privacy of the channel
+         *
+         * @param $privacy
+         */
         public function setPrivacy($privacy) {
             if (in_array($privacy, Array(self::PRIVACY_PRIVATE, self::PRIVACY_PUBLIC, self::PRIVACY_SECRET))) {
                 $this->privacy = $privacy;
             }
         }
+
+
 
     }
